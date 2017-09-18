eccaa7c87dcd516a2247b0be44e9f3da6e6264a8
Fadojutimi Temitayo Olusegun <Adesege@Adesege.local>
2017-09-18 21:46:13 +0100

    bug(bug-fix): fix heroku deployment
    - add more gulp tasks
    - change Procfile web process config
    
    [Fixes]

--- gulpfile.js
+++ gulpfile.js
@@ -9,45 +9,70 @@ bower = require('gulp-bower');
 require('dotenv').config();
 
 var PORT = process.env.PORT;
+var NODE_ENV = process.env.NODE_ENV;
 
 gulp.task('jade', function(){
     gulp.src('app/views/**')
       .pipe(gulp.dest('build/app/views'));
   });
 
-  gulp.task('js', ['jade'], function(){
+  gulp.task('js-public', ['jade'], function(){
     gulp.src('public/js/**')
-    .pipe(gulp.dest('build/public/js'))
-    .gulp.src('app/**/*.js')
+    .pipe(gulp.dest('build/public/js'));
+  });
+
+  gulp.task('js-app', ['jade'], function(){
+    gulp.src('app/**/*.js')
       .pipe(gulp.dest('build/app'));
   });
 
-  gulp.task('html', ['jade', 'js'], function(){
+  gulp.task('html', ['jade', 'js-public', 'js-app'], function(){
     gulp.src('public/views/**')
     .pipe(gulp.dest('build/public/views'));
   });
 
   gulp.task('sass', function(){
-    gulp.src('public/css/*.scss')
+    gulp.src('public/css/**.scss')
     .pipe(sass())
-  .pipe(gulp.dest('public/css/'));
+    .pipe(gulp.dest('build/public/css/'));
+  });
+
+  gulp.task('css', function(){
+    gulp.src('public/css/**.css')
+    .pipe(gulp.dest('build/public/css/'));
   });
 
   gulp.task('test', function(){
     return gulp.src('test/**/*.js', { read: false })
       .pipe(mocha({
         reporter: 'spec'
-      }));
+      }))
+      .pipe(gulp.dest('build/test/'));;
+  });
+
+  gulp.task('server', function(){
+    return gulp.src('./server.js')
+      .pipe(gulp.dest('./build'));
   });
 
   gulp.task('bower', function(){
-    return bower({
-      targetDir: './public/lib',
-      layout: 'byComponent',
-      install: true,
-      verbose: true,
-      cleanBowerDir: true
-    });
+    return bower()
+    .pipe(gulp.dest('./build/public/lib'));
+  });
+
+  gulp.task('config', function(){
+    return gulp.src('./config/**')
+    .pipe(gulp.dest('./build/config'));
+  });
+
+  gulp.task('public', function(){
+    return gulp.src('./public/**')
+    .pipe(gulp.dest('./build/public'));
+  });
+
+  gulp.task('app', function(){
+    return gulp.src('./app/**')
+    .pipe(gulp.dest('./build/app'));
   });
 
   gulp.task('nodemon', function(){
@@ -69,6 +94,17 @@ gulp.task('jade', function(){
     gulp.watch('public/css/**', ['sass']);
   });
 
-  gulp.task('default', ['nodemon']);
+  switch(NODE_ENV){
+    case "development":
+      gulp.task('default', ['nodemon']);
+    break;
+    case "production":
+      gulp.task('default', ['jade', 'js-public', 'js-app', 'sass', 'server', 'html', 'css', 'config', 'app', 'public']);
+    break;
+    default:
+    gulp.task('default', ['nodemon']);
+    break;
+  }
+  
   gulp.task('install', ['bower']);
   
