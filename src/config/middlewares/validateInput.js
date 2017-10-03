92dc200353697c0427c9ec3a4d14401f78cce07b
temiooo <temitope.joloko@gmail.com>
2017-10-03 17:14:00 +0100

    bug(bug-fix): proper validation of user details on signup

--- src/config/middlewares/validateInput.js
+++ src/config/middlewares/validateInput.js
@@ -4,24 +4,24 @@ import { isEmpty, trim } from 'lodash';
 const validateInput = (input) => {
   let error = '';
 
+  if (input.password.length === 0) {
+    error = 'Password is Required';
+  } else if (input.password.length < 5) {
+    error = 'Password is too short (Minimum of 5 characters required)';
+  }
+
   if (trim(input.email).length === 0) {
-    error = 'Email is Invalid';
+    error = 'Email is Required';
   } else if (!validator.isEmail(input.email)) {
     error = 'Email is Invalid';
   }
 
   if (trim(input.name).length === 0) {
-    error = 'Name is Invalid';
+    error = 'Name is Required';
   } else if (input.name.charAt(0) === ' ') {
     error = 'Name is Invalid';
   } else if (input.name.charAt(input.name.length - 1) === ' ') {
     error = 'Name is Invalid';
-  }
-
-  if (trim(input.password).length === 0) {
-    error = 'Password is too short';
-  } else if (input.password.length < 5) {
-    error = 'Password is too short';
   }
 
   return {
