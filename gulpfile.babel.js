522d99aafa77908d2ff30b3702e2505b80939966
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2017-10-04 13:29:11 +0100

    refactor(merge-conflict): resolve merge conflict with remote staging

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -82,12 +82,15 @@ gulp.task('move_json', () =>
     .src(paths.configJson)
     .pipe(gulp.dest('./build/config')));
 
+<<<<<<< HEAD
 gulp.task('move_svg', () =>
   gulp
     .src(paths.svg)
     .pipe(gulp.dest('./build/public/svg')));
 
 
+=======
+>>>>>>> 0037fd08130100104e00f84c48361abb241f6371
 gulp.task('babel', ['lint'], () => {
   gulp
     .src(paths.allJs)
