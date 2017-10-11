f70c1388ab56fe25f5a9782036582906084aa6b1
nosisky <nosisky@gmail.com>
2017-10-11 14:02:02 +0100

    Fix merge conflict

--- src/public/js/controllers/log.js
+++ src/public/js/controllers/log.js
@@ -2,11 +2,7 @@ angular.module('mean.system')
 .controller('LogController', ['$scope', 'Global', 'game', '$timeout', '$http', '$window', '$location','$dialog', function ($scope, Global, game, $timeout, $http, $window, $location, $dialog) {
   $scope.show = 1;
   $scope.games = [];
-<<<<<<< HEAD
   $scope.gameRank = [];
-=======
-
->>>>>>> staging
   $scope.abandonGame = () => {
     console.log($location.path('/'))
   };
@@ -17,7 +13,6 @@ angular.module('mean.system')
   setHttpHeader();
   $http.get('/api/games/logs')
   .then((res) => {
-<<<<<<< HEAD
     for (let i = 0; i < res.data.length; i += 1) {
       $scope.games.push(res.data[i]);
     }
@@ -28,12 +23,3 @@ angular.module('mean.system')
     $scope.gameRank = response.data.data;
   })
 }]);
-=======
-    console.log(res.data);
-    for (let i = 0; i < res.data.length; i += 1) {
-      $scope.games.push(res.data[i]);
-    }
-    console.log($scope.games);
-  });
-}]);
->>>>>>> staging
